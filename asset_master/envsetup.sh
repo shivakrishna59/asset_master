46573270cf4c55ab2e44e30e150dec92d59bd67c
shivakrishna59 <shivakrishnachikkudu1122@gmail.com>
2023-08-30 00:13:10 +0530

    ci cd

---
+++ asset_master/envsetup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -d "venv" ]
+then
+    echo "Python virtual environment exists."
+else
+    python3 -m venv venv
+fi
+
+bat script: venv/scripts/activate
+
+
+pip3 install -r requirements.txt
+
+if [ -d "logs" ]
+then
+    echo "Log folder exists."
+else
+    mkdir logs
+    touch logs/error.log logs/access.log
+fi
+
+sudo chmod -R 777 logs
+echo "venvsetup finishes"
---
+++ asset_master/initial-setup.sh
@@ -0,0 +1,17 @@
+sudo apt-get update -y
+
+wget -q -0 - https://pkg.jenkins.io/debain-stable/jenkins.io.key |
+  sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debain-stable binary/ > \
+/etc/apt/sources.list.d/jenkins.list'
+
+sudo apt-get update
+sudo apt-get install jenkins
+
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+
+sudo systemctl start nginx
+sudo systemctl enable nginx
+sudo systemctl enable jenkins
+sudo systemctl status jenkins
